511d0dca37d5087521040e638cb39ab000b0b677
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-13 22:11:43 +0530

    Created method to generate random color.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react';
+
 import {
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
 
 function App(): JSX.Element {
 
+  const [randomBackground, setRandomBackground] = useState('#ffffff')
+
+  const generateRandomColor = () => {
+
+    const hexRange = '0123456789ABCDEF';
+    let hexColor = '#';
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hexRange[Math.floor(Math.random() * 16)]
+    }
+    setRandomBackground(hexColor);
+
+  }
+
   return (
 
-    <View >
-      <Text>Random Color Generator</Text>
+    <View>
+      <View>
+        <TouchableOpacity onPress={generateRandomColor}>
+          <Text>Change Background</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
