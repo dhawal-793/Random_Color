f512a624e5d15f2601a30089c565ea5354742242
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-13 22:07:52 +0530

    Initialized an Empty React-Native project.

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+  return (
+
+    <View >
+      <Text>Random Color Generator</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
